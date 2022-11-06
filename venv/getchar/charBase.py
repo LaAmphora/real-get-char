b08f0f7d6811bbdc2a5d2cf78b885b297d886f98
shopiahacks <shopiamartinn@gmail.com>
2022-11-05 23:53:41 -0500

    goodluck

--- venv/getchar/charBase.py
+++ venv/getchar/charBase.py
@@ -1,6 +1,19 @@
-from flask import Flask
+from flask import Flask, request, render_template
  
 app = Flask(__name__)
 
+@app.route('/paw', methods =["GET", "POST"])
+def pawprint():
+    if request.method == "POST":
+       pawprint = request.form.get("pp")
+       return pawprint + " successfully logged in."
+    return render_template("login.html")
+ 
+
+@app.route('/')      
+def base():
+    return render_template("base.html")
+
+
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
